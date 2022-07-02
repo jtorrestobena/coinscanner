13ff81193a387aaa5559bb2ba6e9b51b35f24f5a
Jose Torres <josepantoni.torres@gmail.com>
2022-07-02 18:31:37 +0200

    Update project

--- app/src/test/java/com/bytecoders/coinscanner/ui/home/HomeViewModelTest.kt
+++ app/src/test/java/com/bytecoders/coinscanner/ui/home/HomeViewModelTest.kt
@@ -1,26 +1,16 @@
 package com.bytecoders.coinscanner.ui.home
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
-import com.bytecoders.coinscanner.service.coingecko.MarketItem
-import com.bytecoders.coinscanner.util.BaseCoroutineTest
 import com.bytecoders.coinscanner.util.getOrAwaitValue
-import junit.framework.Assert.assertEquals
-import junit.framework.Assert.assertNotNull
-import junit.framework.TestCase
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.test.TestCoroutineDispatcher
-import kotlinx.coroutines.test.TestCoroutineScope
-import kotlinx.coroutines.test.runBlockingTest
-import kotlinx.coroutines.test.setMain
-import org.hamcrest.CoreMatchers.equalTo
-import org.hamcrest.CoreMatchers.not
-import org.hamcrest.MatcherAssert.assertThat
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertNotNull
 import org.junit.Rule
 import org.junit.Test
 
-@ExperimentalCoroutinesApi
-class HomeViewModelTest: BaseCoroutineTest() {
+class HomeViewModelTest {
+    @get:Rule
+    val instantExecutorRule = InstantTaskExecutorRule()
+
     private val viewModel = HomeViewModel()
 
     @Test
