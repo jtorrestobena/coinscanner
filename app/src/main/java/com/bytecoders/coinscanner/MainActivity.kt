880cce031a549671e0da99fc48280dddf66aebe3
Jose Torres <joseantonio.torrestobena@helsana.ch>
2022-07-08 11:20:40 +0200

    Use material them

--- app/src/main/java/com/bytecoders/coinscanner/MainActivity.kt
+++ app/src/main/java/com/bytecoders/coinscanner/MainActivity.kt
@@ -2,12 +2,12 @@ package com.bytecoders.coinscanner
 
 import MainScreen
 import android.os.Bundle
+import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.appcompat.app.AppCompatActivity
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class MainActivity : AppCompatActivity() {
+class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
--- app/src/main/java/com/bytecoders/coinscanner/ui/main/MainScreen.kt
+++ app/src/main/java/com/bytecoders/coinscanner/ui/main/MainScreen.kt
@@ -4,17 +4,20 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.compose.rememberNavController
 import com.bytecoders.coinscanner.ui.navigation.BottomNavigationView
 import com.bytecoders.coinscanner.ui.navigation.NavigationGraph
+import com.bytecoders.coinscanner.ui.theme.AppTheme
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MainScreen() {
-    val navController = rememberNavController()
-    Scaffold(
-        bottomBar = { BottomNavigationView(navController = navController) }
-    ) { paddingValues ->
-        NavigationGraph(
-            navController = navController,
-            bottomPadding = paddingValues.calculateBottomPadding()
-        )
+    AppTheme {
+        val navController = rememberNavController()
+        Scaffold(
+            bottomBar = { BottomNavigationView(navController = navController) }
+        ) { paddingValues ->
+            NavigationGraph(
+                navController = navController,
+                bottomPadding = paddingValues.calculateBottomPadding()
+            )
+        }
     }
 }
