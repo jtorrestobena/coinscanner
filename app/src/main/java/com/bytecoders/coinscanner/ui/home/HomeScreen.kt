0f2a02b8d1d01c10fdcdcc9f2562bc79e9de3b8a
Jose Torres <josepantoni.torres@gmail.com>
2022-07-24 08:28:56 +0200

    Click all options on MainScreenTest

--- app/src/main/java/com/bytecoders/coinscanner/ui/home/HomeScreen.kt
+++ app/src/main/java/com/bytecoders/coinscanner/ui/home/HomeScreen.kt
@@ -15,6 +15,7 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextOverflow
@@ -176,12 +177,14 @@ fun CoinItemPreview() {
     )
 }
 
+const val COIN_ITEM = "COIN_ITEM"
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun CoinItem(coin: MarketItem, currency: String, modifier: Modifier) {
     Card(
         modifier = modifier
             .fillMaxWidth()
+            .testTag(COIN_ITEM)
     ) {
         ConstraintLayout(
             modifier = Modifier
