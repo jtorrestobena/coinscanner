32c2f50a6da63a88bad3e41a7a0c232140320f88
Jose Torres <josepantoni.torres@gmail.com>
2022-07-24 14:46:32 +0200

    Added save state handle

--- app/src/main/java/com/bytecoders/coinscanner/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/bytecoders/coinscanner/ui/home/HomeViewModel.kt
@@ -1,11 +1,15 @@
 package com.bytecoders.coinscanner.ui.home
 
+import android.os.Parcelable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
+import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import androidx.paging.*
+import androidx.versionedparcelable.ParcelField
+import androidx.versionedparcelable.VersionedParcelize
 import com.bytecoders.coinscanner.currency.CurrencyManager
 import com.bytecoders.coinscanner.data.coingecko.MarketItem
 import com.bytecoders.coinscanner.repository.CoinGeckoRepository
@@ -13,23 +17,28 @@ import com.bytecoders.coinscanner.repository.CoinMarketConfiguration
 import com.bytecoders.coinscanner.service.coingecko.GeckoOrder
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.Flow
+import kotlinx.parcelize.Parcelize
 import java.util.*
 import javax.inject.Inject
 
 const val ITEMS_PER_PAGE = 50
 
+private const val HOME_UI_STATE = "HOME_UI_STATE"
+
+@Parcelize
 data class HomeUiState(
     val marketOrdering: GeckoOrder = GeckoOrder.MARKET_CAP_DESC,
     val currency: Currency = CurrencyManager.getDefaultCurrency(),
     val isRefreshing: Boolean = false
-)
+): Parcelable
 
 @HiltViewModel
 class HomeViewModel @Inject constructor(
-    private val coinGeckoRepository: CoinGeckoRepository
+    private val coinGeckoRepository: CoinGeckoRepository,
+    private val savedStateHandle: SavedStateHandle,
 ) : ViewModel() {
 
-    var uiState by mutableStateOf(HomeUiState())
+    var uiState by mutableStateOf(savedStateHandle[HOME_UI_STATE] ?: HomeUiState())
         private set
 
     private val marketConfiguration: CoinMarketConfiguration
@@ -47,12 +56,17 @@ class HomeViewModel @Inject constructor(
     ).flow.cachedIn(viewModelScope)
 
     fun changeOrder(newOrdering: GeckoOrder) {
-        uiState = uiState.copy(marketOrdering = newOrdering)
+        updateState(uiState.copy(marketOrdering = newOrdering))
         coinGeckoRepository.updateConfiguration(marketConfiguration)
     }
 
     fun changeCurrency(newCurrency: Currency) {
-        uiState = uiState.copy(currency = newCurrency)
+        updateState(uiState.copy(currency = newCurrency))
         coinGeckoRepository.updateConfiguration(marketConfiguration)
+    }
+
+    private fun updateState(newUiState: HomeUiState) {
+        uiState = newUiState
+        savedStateHandle[HOME_UI_STATE] = newUiState
     }
 }
